9e9308b3b50cfc4de2250e2146779bf771f3a115
Augusto Alves <augusto.alves@sou.unifeob.edu.br>
2026-05-06 22:27:52 -0300

    Revert "Add files via upload"
    This reverts commit bcff17377aaca92b5df800d92aaea28b8fb9c27c.

--- model/filme.py
+++
@@ -1,6 +0,0 @@
-class Filme:
-    def __init__(self, nome: str, duracao: str, classificacao: str, id: int = None):
-        self.id = id
-        self.nome = nome
-        self.duracao = duracao
-        self.classificacao = classificacao
